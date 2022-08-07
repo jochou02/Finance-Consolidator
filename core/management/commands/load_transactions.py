e055cf96f8d3062d790cb0d1c6e3056cc4bfa15e
John Chou <jochou02@gmail.com>
2022-08-07 13:53:11 -0700

    Script finished, rendering charts now

--- core/management/commands/load_transactions.py
+++ core/management/commands/load_transactions.py
@@ -1,4 +1,5 @@
-import csv
+import csv, os
+from itertools import islice
 from datetime import date
 from lib2to3.pytree import Base
 from django.conf import settings
@@ -8,6 +9,88 @@ from core.models import Transaction
 class Command(BaseCommand):
     help = 'Load transactions'
 
+
+    def getDate(self, row):
+        date_split = row[0].split('/') 
+        year, day, month = int(date_split[2]), int(date_split[1]), int(date_split[0])
+        dt = date(year, month, day)
+        return dt
+
+    def getCategoryAmex(self, row):
+        raw = row[10]
+        if 'Restaurant' in raw or 'Groceries' in raw:
+            return 'Food/Dining'
+        elif 'Merchandise' in raw:
+            return 'General Merchanise'
+        elif 'Services' in raw:
+            return 'General Services'
+        elif 'Transportation' in raw:
+            return 'Transportation/Travel'
+        else:
+            return 'Other'
+    
+    def getCategoryDiscover(self, row):
+        raw = row[4]
+        if 'Entertainment'in raw:
+            return 'Entertainment'
+        elif 'Merchandise' in raw:
+            return 'General Merchanise'
+        elif 'Services' in raw:
+            return 'General Services'
+        else:
+            return 'Other'
+        
+
     def handle(self, *args, **kwargs):
+
         statement_directory = settings.BASE_DIR / 'Statements'
-        print('test')
+        for filename in os.listdir(statement_directory):
+
+            current_CSV = f'{statement_directory}/{filename}'
+            with open(current_CSV, 'r') as f:
+            
+                # Default reader that skips CSV heading
+                reader = csv.reader(islice(f, 1, None)) 
+                
+                if 'Wells' in filename:
+                    # Wells Fargo CSV has no heading, needs special reader
+                    reader = csv.reader(f)
+                    for row in reader:
+                        dt = self.getDate(row)
+                        merchant = row[4]
+                        amount = row[1]
+                        # Wells Fargo does not categorize transactions
+                        Transaction.objects.get_or_create(
+                            date=dt,
+                            merchant=merchant,
+                            amount=amount
+                        )
+ 
+                elif 'Amex' in filename:
+                    for row in reader:
+                        dt = self.getDate(row)
+                        merchant = row[4]
+                        amount = row[2]
+                        category = self.getCategoryAmex(row)
+                        Transaction.objects.get_or_create(
+                            date=dt,
+                            merchant=merchant,
+                            amount=amount,
+                            category=category
+                        )
+                        
+                elif 'Discover' in filename:
+                    for row in reader:
+                        dt = self.getDate(row)
+                        merchant = row[2]
+                        amount = row[3]
+                        Transaction.objects.get_or_create(
+                            date=dt,
+                            merchant=merchant,
+                            amount=amount,
+                            category=category
+                        )
+                        
+    
+
+                        
--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import *
+import plotly.express as px
 
 # Create your views here.
 
-def PlaceHolder(request):
+def chart(request):
 
-    return
+    transactions = Transaction.objects.all()
+
+    fig = px.line(
+        x=[t.date for t in transactions],
+        y=[t.amount for t in transactions]
+    )
+
+    chart = fig.to_html()
+
+    context = {'chart': chart}
+
+    return render(request, 'core/chart.html', context)
